742b18230af448643ee18288e7de1c12ee3c1a2b
Greem <55126412+GreemJa@users.noreply.github.com>
2023-12-18 13:42:39 +0400

    Move DeviceSpawner and add Device logic

---
+++ Assets/CodeBase/UI/Elements/Device.cs
@@ -0,0 +1,38 @@
+﻿using CodeBase.StaticData.Device;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Elements
+{
+    public class Device : MonoBehaviour
+    {
+        public GameObject Background;
+        public Image Icon;
+        
+        public DeviceState DeviceState
+        {
+            get => _deviceState;
+            set
+            {
+                _deviceState = value;
+                OnStateChanged();
+            }
+        }
+
+        private DeviceState _deviceState;
+        private DeviceConfig _deviceConfig;
+
+        public void Initialize(DeviceState deviceState, DeviceConfig deviceConfig)
+        {
+            DeviceState = deviceState;
+            _deviceConfig = deviceConfig;
+        }
+
+        void OnStateChanged()
+        {
+            Icon.sprite = _deviceConfig.Sprite;
+            Icon.gameObject.SetActive(_deviceState == DeviceState.Filled || _deviceState == DeviceState.Сonnected);
+            Background.SetActive(_deviceState != DeviceState.Сonnected);
+        }
+    }
+}
---
+++ Assets/CodeBase/UI/Elements/DeviceSpawner.cs
@@ -0,0 +1,25 @@
+﻿using CodeBase.StaticData.Device;
+using CodeBase.StaticData.Levels;
+using CodeBase.UI.Services.Factory;
+using UnityEngine;
+
+namespace CodeBase.UI.Elements
+{
+    public class DeviceSpawner : MonoBehaviour
+    {
+        public DeviceTypeId DeviceTypeId;
+        public DeviceState DeviceState;
+
+        private IUIFactory _uiFactory;
+
+        public void Construct(IUIFactory uiFactory)
+        {
+            _uiFactory = uiFactory;
+        }
+        
+        public void Spawn(DeviceSpawnerData deviceData)
+        {
+            _uiFactory.CreateDevice(deviceData);
+        }
+    }
+}
